bc1d02c860f7004810c5f7532a1954f0141b6197
Son Pham-Ba <son.phamba@epfl.ch>
2026-03-06 15:07:44 +0100

    chore: move runai installation from Dockerfile to start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -6,5 +6,16 @@ if [ -n "$SSH_PRIVATE_KEY" ]; then
 	unset SSH_PRIVATE_KEY
 fi
 
+curl -sLo /tmp/runai https://rcp-caas-prod.rcp.epfl.ch/cli/linux \
+	&& install /tmp/runai /usr/local/bin/runai \
+	&& rm /tmp/runai
+
+mkdir -p ~/.kube
+if [ ! -f ~/.kube/config ]; then
+	curl https://wiki.rcp.epfl.ch/public/files/kube-config.yaml -o ~/.kube/config && chmod 600 ~/.kube/config
+fi
+
+# Must run `runai login` manually. Requires web browser interaction
+
 rclone config create jumphost.rcp.epfl.ch sftp host=jumphost.rcp.epfl.ch key_file=/root/.ssh/id_ed25519
 uvicorn --host=0.0.0.0 --timeout-keep-alive=0 api.main:app --reload
